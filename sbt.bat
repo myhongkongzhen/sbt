2231f8711b24f1aca33664462d1ab9dd9bbbbab9
Z_Z.W <myhongkongzhen@gmail.com>
2017-07-06 22:14:13 +0800

    update config file

--- sbt.bat
+++ sbt.bat
@@ -1,3 +1,4 @@
 set SCRIPT_DIR=%~dp0
+set FN=%SCRIPT_DIR%sbtconfig.txt
 set SBT_OPTS="-Dsbt.override.build.repos=true"
 java -Xms512M -Xmx1536M "-Dsbt.override.build.repos=true" -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
