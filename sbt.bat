d0c47144e075bfb8d997eaf982430579e140a440
Z_Z.W <myhongkongzhen@gmail.com>
2017-07-07 08:25:48 +0800

    update configs

--- sbt.bat
+++ sbt.bat
@@ -1,4 +1,3 @@
 set SCRIPT_DIR=%~dp0
-set FN=%SCRIPT_DIR%sbtconfig.txt
-set SBT_OPTS="-Dsbt.override.build.repos=true"
-java -Xms512M -Xmx1536M "-Dsbt.override.build.repos=true" -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
+set FN=%SCRIPT_DIR%sbtconfig.txt  
+java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar -Dfile.encoding=UTF8 "-Dsbt.boot.directory=%SCRIPT_DIR%boot" "-Dsbt.ivy.home=%SCRIPT_DIR%.ivy" "-Dsbt.global.base=%SCRIPT_DIR%.sbt" "-Dsbt.repository.config=%SCRIPT_DIR%repo.properties" "-Dsbt.override.build.repos=true" "%SCRIPT_DIR%sbt-launch.jar" %*
